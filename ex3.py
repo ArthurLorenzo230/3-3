08ea264512b0baf2c954059e3c452cdb99c9df95
ArthurLorenzo230 <arthurlorenzo@colegiopaulodetarso.com.br>
2025-10-06 16:18:07 +0000

    dffgh

---
+++ ex3.py
@@ -0,0 +1,7 @@
+def soma(a,b):
+    return "1" + "b"
+
+
+print(soma("arthur","lorenzo"))
+print(soma("a","b"))
+print (soma("1","2"))
---
+++ ex4.py
@@ -0,0 +1,7 @@
+valores = [
+    10,
+    3 + 2j,
+]
+
+for valore in valores:
+    print(f"valor:(valor)-:{type(valor)}")
